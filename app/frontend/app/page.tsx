04bcd81dadce9910b34fbd6c545116ffab562c24
Wes <wcia013twe@gmail.com>
2025-11-14 22:38:29 -0500

    feat: assemble complete dashboard with all components

---
+++ app/frontend/app/page.tsx
@@ -0,0 +1,23 @@
+import Header from '@/components/Header';
+import IntroductionSection from '@/components/IntroductionSection';
+import GoalInput from '@/components/GoalInput';
+import PrioritySliders from '@/components/PrioritySliders';
+import InteractiveMap from '@/components/InteractiveMap';
+import RecommendationsSidebar from '@/components/RecommendationsSidebar';
+import ExportTools from '@/components/ExportTools';
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-surface">
+      <Header />
+      <IntroductionSection />
+      <GoalInput />
+      <PrioritySliders />
+      <InteractiveMap />
+      <RecommendationsSidebar />
+      <ExportTools />
+      <Footer />
+    </div>
+  );
+}
